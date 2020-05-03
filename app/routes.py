540b6a9f5a3c533b5c0f86efd01cf093f9277f6e
Brenda-M <brendanjuguna1@gmail.com>
2020-05-03 03:26:31 +0300

    add logout route

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from app import app, bcrypt, db
 from app.forms import RegistrationForm, LoginForm
 from app.models import User, Post
+from flask_login import login_user, current_user,logout_user
 
 
 
@@ -22,6 +23,8 @@ def about():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+  if current_user.is_authenticated:
+    return redirect(url_for('index'))
   form = RegistrationForm()
   if form.validate_on_submit():
     hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -34,13 +37,21 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+  if current_user.is_authenticated:
+    return redirect(url_for('index'))
   form = LoginForm()
   if form.validate_on_submit():
-    if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-      flash(f'You have been logged in', 'success')
-      return redirect(url_for('index'))
+    user = User.query.filter_by(email=form.email.data).first()
+    if user and bcrypt.check_password_hash(user.password, form.password.data):
+      login_user(user, remember=form.remember.data)
+      return redirect(url_for('index'))   
     else:
-      flash('Login Unsuccessful. Please check email and password', 'success')
+      flash('Login Unsuccessful. Please check email and password', 'danger')
 
   return render_template('login.html', title='Login', form=form)
+
+@app.route('/logout')
+def logout():
+  logout_user()
+  return redirect (url_for('index'))
 
