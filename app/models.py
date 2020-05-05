09cf29d8a4d3063e7bb898da32534aa406a29b98
Brenda-M <brendanjuguna1@gmail.com>
2020-05-05 05:17:59 +0300

    add a downvote class to keep track of the number of downvotes associated with a post

--- app/models.py
+++ app/models.py
@@ -70,9 +70,9 @@ class Upvote(db.Model):
   __tablename__ = 'upvotes'
 
   id = db.Column(db.Integer, primary_key=True)
-  upvote = db.Column(db.Integer, default=0)
-  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-  pitch_id = db.Column(db.Integer, db.ForeignKey('post.id'))
+  upvote = db.Column(db.Integer, default = 0)
+  user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+  pitch_id = db.Column(db.Integer, db.ForeignKey('pitches.id'))
 
   def save_upvotes(self):
     db.session.add(self)
@@ -95,4 +95,39 @@ class Upvote(db.Model):
 
   def __repr__(self):
     return f'{self.user_id}:{self.pitch_id}'
+
+class Downvote(db.Model):
+
+  __tablename__ = 'downvotes'
+
+  id = db.Column(db.Integer, primary_key=True)
+  downvote = db.Column(db.Integer, default=0)
+  pitch_id = db.Column(db.Integer, db.ForeignKey('pitches.id'))
+  user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+  def save_downvotes(self):
+    db.session.add(self)
+    db.session.commit()
+
+
+  def add_downvotes(cls, id):
+    downvote_pitch = Downvote(user = current_user, pitch_id=id)
+    downvote_pitch.save_downvotes()
+
+  
+  @classmethod
+  def get_downvotes(cls, id):
+    downvote = Downvote.query.filter_by(pitch_id=id).all()
+    return downvote
+
+  @classmethod
+  def get_all_downvotes(cls, pitch_id):
+    downvote = Downvote.query.order_by('id').all()
+    return downvote
+
+  def __repr__(self):
+    return f'{self.user_id}:{self.pitch_id}'
+
+
+
 
